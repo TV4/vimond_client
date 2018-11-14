ec760b409765aa8d71013b01d185e2ea8df61cc5
Rubikskub <wwwadm@tv4.se>
2018-11-14 10:06:40 +0100

    Fix formatting

--- test/support/http_client.ex
+++ test/support/http_client.ex
@@ -1,3 +1,4 @@
 defmodule HTTPClient do
-  @callback request(method :: atom(), url :: String.t(), options :: Keyword.t()) :: HTTPotion.Response.t() | HTTPotion.ErrorResponse.t()
+  @callback request(method :: atom(), url :: String.t(), options :: Keyword.t()) ::
+              HTTPotion.Response.t() | HTTPotion.ErrorResponse.t()
 end
