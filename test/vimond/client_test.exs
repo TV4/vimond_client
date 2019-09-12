a4725d8362c55fc4d84c244ae0e83b2fcf9202ec
Axolotl Mob <axolotl@b17g.services>
2019-09-12 09:21:59 +0200

    Add client test for invalid JSON

--- test/vimond/client_test.exs
+++ test/vimond/client_test.exs
@@ -10,6 +10,13 @@ defmodule Vimond.ClientTest do
                {:error, %{type: :http_error, source_errors: ["some error"]}}
     end
 
+    test "with unexpected json" do
+      response = %HTTPotion.Response{body: "bad json", headers: %HTTPotion.Headers{}}
+
+      assert handle_response(response, :never_called) ==
+               {:error, %{type: :bad_vimond_response, source_errors: ["Could not parse Vimond response"]}}
+    end
+
     test "with a successful response" do
       response = %HTTPotion.Response{
         body: Jason.encode!(%{"code" => "SESSION_AUTHENTICATED"}),
