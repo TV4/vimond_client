e60e3541219fbffd6b36550c2bf341d9ef0e3cea
Karl Eklund <localpart@gmail.com>
2021-05-19 15:51:35 +0200

    Add Vimond.Config.from_environment helper

--- lib/vimond/config.ex
+++ lib/vimond/config.ex
@@ -2,4 +2,13 @@ defmodule Vimond.Config do
   defstruct ~w(base_url api_key api_secret)a
 
   @type t :: %__MODULE__{}
+
+  @doc "Read configuration from environment. Helper for manual testing of Vimond Client"
+  def from_environment() do
+    %__MODULE__{
+      base_url: System.fetch_env!("VIMOND_BASE_URL"),
+      api_key: System.fetch_env!("VIMOND_API_KEY"),
+      api_secret: System.fetch_env!("VIMOND_API_SECRET")
+    }
+  end
 end
