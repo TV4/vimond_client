83012f41a5a8796698bd3a94fb07ec55dd35af65
Karl Eklund <localpart@gmail.com>
2021-06-06 23:04:04 +0200

    add type to Vimond.Session

--- lib/vimond/session.ex
+++ lib/vimond/session.ex
@@ -1,4 +1,5 @@
 defmodule Vimond.Session do
+  @type t :: %__MODULE__{}
   defstruct ~w(expires vimond_authorization_token vimond_remember_me vimond_jsessionid)a
   defdelegate get_and_update(session, key, fun), to: Map
 end
